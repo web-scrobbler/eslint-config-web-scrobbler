d87a84749344a196326f8fc6caaa4fb9e34d0308
Jacob MacDonald <jaccarmac@gmail.com>
2025-09-21 18:20:53 -0500

    fix hanging consts

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import baseRules from './config/base-rules';
 import jsOverrides from './config/javascript-overrides';
 
-const baseRules = require('./config/base-rules');
-const jsOverrides = require('./config/javascript-overrides');
-
 export default [...baseRules, jsOverrides];
